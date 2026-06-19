351ad24029151fa3d4a4ad5642628d418fb0680a
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-06-19 17:56:11 +0530

    Add Streamlit app integration with Ollama model and  create first generative ai appliction by using the ollama

---
+++ ollama/app.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the question asked"),
+        ("user", "Question: {question}")
+    ]
+)
+
+# Streamlit UI
+st.title("LangChain Demo With Llama 3.2")
+
+input_text = st.text_input("What question do you have in mind?")
+
+# Ollama Model
+llm = Ollama(model="llama3.2")
+
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question": input_text}))
